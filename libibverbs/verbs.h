84040ebdbc4f1ca8ebeb418ed185893e6096451c
Matan Barak <matanb@mellanox.com>
2017-10-24 15:48:11 +0300

    libibverbs: Add flow steering egress support
    issue: 1087238
    Change-Id: Ifa9cabaa87676054ea8ea242a6fdfcd80fa46eff
    Signed-off-by: Matan Barak <matanb@mellanox.com>

--- libibverbs/verbs.h
+++ libibverbs/verbs.h
@@ -1232,6 +1232,7 @@ struct ibv_ah {
 enum ibv_flow_flags {
 	IBV_FLOW_ATTR_FLAGS_ALLOW_LOOP_BACK = 1 << 0,
 	IBV_FLOW_ATTR_FLAGS_DONT_TRAP = 1 << 1,
+	IBV_FLOW_ATTR_FLAGS_EGRESS = 1 << 2,
 };
 
 enum ibv_flow_attr_type {
